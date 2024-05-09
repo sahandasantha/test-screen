f126f21c306fc62eef1f19541c70041e158d31d0
Sahan Dasantha <68588079+sahandasantha@users.noreply.github.com>
2024-05-10 01:55:03 +0300

    Update update.php

--- update.php
+++ update.php
@@ -8,10 +8,16 @@ if (isset($_GET['version']) && !empty($_GET['version']))
   if($version == $current_version)
   {
     $data = "true";
+    header("Access-Control-Allow-Origin: *");
+ header('Content-Type: application/json; charset=utf-8');
+ echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
   }
   else
   {
     $data = "false";
+    header("Access-Control-Allow-Origin: *");
+ header('Content-Type: application/json; charset=utf-8');
+ echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
   }
 }
 else
