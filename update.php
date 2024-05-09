5d5c193e58d092d5cf88dd11d3c4c310c43e5e71
Sahan Dasantha <68588079+sahandasantha@users.noreply.github.com>
2024-05-10 02:01:03 +0300

    Update update.php

--- update.php
+++ update.php
@@ -1,26 +1,8 @@
 <?php
 
-$current_version = "2.2";
+echo "dddd";
 
-if (isset($_GET['version']) && !empty($_GET['version']))
-{
-  $version = $_GET['version'];
-  if($version == $current_version)
-  {
-    $data = "true";
-    echo $data;
-  }
-  else
-  {
-    $data = "false";
-    echo $data;
-  }
-}
-else
-{
-  $data = "false";
-  echo $data;
-}
+
 
   
 ?>
