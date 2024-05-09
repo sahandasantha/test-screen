ed4c444a060ece7896a69e8eb8e2ad1a1e5677de
Sahan Dasantha <68588079+sahandasantha@users.noreply.github.com>
2024-05-10 01:54:39 +0300

    Update update.php

--- update.php
+++ update.php
@@ -7,12 +7,20 @@ if (isset($_GET['version']) && !empty($_GET['version']))
   $version = $_GET['version'];
   if($version == $current_version)
   {
-    echo "true";
+    $data = "true";
   }
   else
   {
-    echo "false";
+    $data = "false";
   }
 }
+else
+{
+  $data = "false";
+  header("Access-Control-Allow-Origin: *");
+ header('Content-Type: application/json; charset=utf-8');
+ echo json_encode($data, JSON_UNESCAPED_UNICODE | JSON_FORCE_OBJECT | JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT);
+}
 
+  
 ?>
